15d8f2f52eb770dc1c6a9b2193d5ab00c8c866f9
Lisa Williams <lisaismyname0@gmail.com>
2022-06-24 13:31:50 -0400

    still toying with how to handle aces, otherwise consolidated code a bit)

--- blackjack2.py
+++ blackjack2.py
@@ -1,7 +1,9 @@
 import random
 
-CARDVALUE_PAIRS = {"Ace": [1, 11], "Two": 2, "Three": 3, "Four": 4, "Five": 5, "Six": 6,
-                   "Seven": 7, "Eight": 8, "Nine": 9, "Ten": 10, "Jack": 10, "Queen": 10, "King": 10}
+CARDVALUE_PAIRS = {
+    "Ace": [], "Two": 2, "Three": 3, "Four": 4, "Five": 5, "Six": 6,
+    "Seven": 7, "Eight": 8, "Nine": 9, "Ten": 10, "Jack": 10, "Queen": 10, "King": 10
+}
 SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
 
 
@@ -12,13 +14,14 @@ class Card:
 
     def show(self):
         if self.point_value[0] == "Ace":
-            self.point_value[1] == input("One or Eleven?")
-            if input == "one" or "One" or 1:
-                self.point_value[1] == 1
+            print("Player drew an Ace. They must decide if it is worth:")
+            input("One point? or Eleven points? ")
+            if input == "one" or input == "One" or input == 1:
+                self.point_value[1] = "1"
                 print(
                     f"{self.point_value[0]} of {self.suit}. Worth: {self.point_value[1]} points")
-            elif input == "Eleven" or "eleven" or 11:
-                self.point_value[1] == 11
+            elif input == "Eleven" or input == "eleven" or input == "11":
+                self.point_value[1] = "11"
                 print(
                     f"{self.point_value[0]} of {self.suit}. Worth: {self.point_value[1]} points")
         else:
@@ -70,28 +73,28 @@ class Player:
     def hit_me(self, deck):
         self.hand.append(deck.draw_a_card())
 
-    def showHand(self):
+    def show_hand(self):
         print(f"{self.name}'s hand")
         for card in self.hand:
             card.show()
 
-    def add_points(self,):
+    def add_points(self):
         point_tally = []
         for card in self.hand:
             point_tally.append(card.point_value[1])
-            print(point_tally)
+        print(point_tally)
 
 
 player1 = Player("PLAYER ONE")
 dealer = Player("DEALER")
+players = [player1, dealer]
 
 
-def play_round_one():
-    player1.initial_deal(deck)
-    dealer.initial_deal(deck)
-    player1.showHand()
-    dealer.showHand()
-    player1.add_points()
+def round_one():
+    for player in players:
+        player.initial_deal(deck)
+        player.show_hand()
+        player.add_points()
     print("End of round one")
 
 
@@ -103,5 +106,5 @@ def round_two():
         player1.showHand()
 
 
-play_round_one()
-round_two()
+round_one()
+# round_two()
